9c9dcb50c4a3c947dfde8ce4f7e514af9f1ed60d
Alex M <contact@stkyfngrs.com>
2023-03-04 13:44:52 -0800

    commit

--- pages/mint1Ok.tsx
+++ pages/mint1Ok.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
 export default function mint1Ok() {
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
-      <h1> We are about to get STKY</h1>
+      <div className="relative w-full h-full flex flex-col items-center justify-center">
+        <Image
+          src="/images/blur.jpeg"
+          alt=""
+          className="animate-pulse-slow absolute inset-auto block w-full min-h-screen object-cover"
+          width={1920}
+          height={1080}
+        />
+
+        <div className="relative w-full h-full flex flex-col items-center justify-center px-2 md:px:10">
+          <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
+            <h1>Coming Soon</h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- pages/mint1of1.tsx
+++ pages/mint1of1.tsx
@@ -11,6 +11,12 @@ export default function Mint1of1() {
           width={1920}
           height={1080}
         />
+
+        <div className="relative w-full h-full flex flex-col items-center justify-center px-2 md:px:10">
+          <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
+            <h1>Coming Soon</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
