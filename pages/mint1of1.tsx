d1305622f05adbbd5831ae7318c64a4eee6c5a6d
Alex M <contact@stkyfngrs.com>
2023-03-04 13:01:07 -0800

    commit

--- pages/mint1of1.tsx
+++ pages/mint1of1.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image'
+
 export default function Mint1of1() {
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
-      <h1> We are about to get STKY</h1>
+      <div className="relative w-full h-full flex flex-col items-center justify-center">
+        <Image src="/images/blur.jpeg" alt="" />
+      </div>
     </div>
   )
 }
