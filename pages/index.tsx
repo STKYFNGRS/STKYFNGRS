8037e3ea47fdec9cba97bf39631ec98e26e8bc79
Alex M <contact@stkyfngrs.com>
2023-03-04 12:14:21 -0800

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,7 +120,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <a href="/mint">
+            <a href="/Mint">
               <div className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
                 Go to 1/1 minting page
                 <svg
