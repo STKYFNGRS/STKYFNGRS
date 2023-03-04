e8751751b7757b521615546b3e6b8c722bbf47fe
Alex M <contact@stkyfngrs.com>
2023-03-04 13:04:32 -0800

    commit

--- pages/mint1of1.tsx
+++ pages/mint1of1.tsx
@@ -4,7 +4,13 @@ export default function Mint1of1() {
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
-        <Image src="/images/blur.jpeg" alt="" />
+        <Image
+          src="/images/blur.jpeg"
+          alt=""
+          className="animate-pulse-slow absolute inset-auto block w-full min-h-screen object-cover"
+          width={1920}
+          height={1080}
+        />
       </div>
     </div>
   )
