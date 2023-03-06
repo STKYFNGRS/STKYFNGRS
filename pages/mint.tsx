85733cce1cff3a5d3f3cdf948d9a53a2380c66be
Alex M <contact@stkyfngrs.com>
2023-03-06 13:36:39 -0800

    commit

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 
-export default function Mint1of1() {
+export default function Mint() {
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
         <Image
           src="/images/silhouette.png"
           alt=""
-          className="animate-pulse-slow absolute inset-auto block w-full min-h-screen object-cover"
-          width={1920}
-          height={1080}
+          className="animate-pulse-slow absolute inset-auto block w-full h-full min-h-screen object-cover"
+          width={1280}
+          height={720}
         />
 
         <div className="relative w-full h-full flex flex-col items-center justify-center px-2 md:px:10">
