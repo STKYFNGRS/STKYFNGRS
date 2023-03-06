bf62132bfe4ddd16a106f0a518fc21679f5d551f
Alex M <contact@stkyfngrs.com>
2023-03-06 12:14:45 -0800

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,7 +123,7 @@ export default function Home() {
             </div>
             <Link href="/mint1of1">
               <div className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
-                Go to 1/1 minting page
+                Go minting page
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-6 h-6 ml-2 mt-0.5"
@@ -140,25 +140,27 @@ export default function Home() {
                 </svg>
               </div>
             </Link>
-            <Link href="/mint1Ok">
-              <div className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
-                Go to 10k minting page
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 ml-2 mt-0.5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </div>
-            </Link>
+            {/* Hidden until ready for 10k minting
+              <Link href="/mint1Ok">
+                <div className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
+                  Go to 10k minting page
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 ml-2 mt-0.5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </svg>
+                </div>
+              </Link>
+            */}
           </div>
         </div>
       </div>
