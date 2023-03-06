4ce2fdd913834c77ee368bce6f69a997fea4c8af
Alex M <contact@stkyfngrs.com>
2023-03-06 13:17:31 -0800

    commit

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -5,7 +5,7 @@ export default function Mint1of1() {
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
         <Image
-          src="/images/Background.jpg"
+          src="/images/silhouette.png"
           alt=""
           className="animate-pulse-slow absolute inset-auto block w-full min-h-screen object-cover"
           width={1920}
