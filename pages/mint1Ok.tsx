ef32bd02fba9cd3295c322beeac77b9700330dbe
Alex M <contact@stkyfngrs.com>
2023-03-05 20:11:51 -0800

    commit

--- pages/mint1Ok.tsx
+++ pages/mint1Ok.tsx
@@ -12,7 +12,7 @@ export default function mint1Ok() {
         />
 
         <div className="relative w-full h-full flex flex-col items-center justify-center px-2 md:px:10">
-          <div className="z-1 md:max-w-3xl w-full bg-blue-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
+          <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
             <h1 className="animate-pulse-slow font-coiny uppercase font-bold text-3xl md:text-4xl bg-clip-text mt-3">
               Coming Soon
             </h1>
