b0f3ad043e9909ac2772b1a38c0ebb6a9e224bd9
Alex M <contact@stkyfngrs.com>
2023-03-07 16:43:26 -0800

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
                   <a
                     href="https://mastodon.social/@STKYFNGRS"
                     target="_blank"
-                    rel="noreferrer"
+                    rel="me"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
