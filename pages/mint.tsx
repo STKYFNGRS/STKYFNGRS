3f11854feba06555b17c0942e2590c149c850c58
Alex M <contact@stkyfngrs.com>
2023-03-11 16:48:44 -0800

    commit

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -4,14 +4,6 @@ export default function Mint() {
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
-        <Image
-          src="/images/silhouette.png"
-          alt=""
-          className="absolute inset-auto block w-full h-full min-h-screen object-cover"
-          width={1280}
-          height={720}
-        />
-
         <div className="relative w-full h-full flex flex-col items-center justify-center px-2 md:px:10">
           <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
             <h1 className="animate-pulse-slow font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br from-brand-green to-brand-oxford-blue bg-clip-text text-transparent mt-3">
