617980c850f438de79607f31097e4a898d3133d5
Alex M <contact@stkyfngrs.com>
2023-04-01 14:04:27 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <p>This is the About page.</p>;
+  return (
+    <div className="about-me-container">
+      <div className="about-me-text">
+        <p>
+          I am a dedicated father of two and a loving husband, with a passion
+          for working as an analyst. My passion for technology and creativity
+          extends to various hobbies, such as PC building, programming,
+          photography, and digital art. I am a self-taught programmer, and I am
+          currently learning how to use next.js and solidity. It is my goal to
+          become a full-stack web3 developer.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
