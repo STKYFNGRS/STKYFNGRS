45dca8fb10dc42b359ce64b59af12f784905bee9
Alex M <contact@stkyfngrs.com>
2023-04-01 16:42:33 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,13 @@ const AboutPage = () => {
     <div className="about-me-container">
       <div className="about-me-text">
         <p>
-          I am a dedicated father of two and a loving husband, with a passion
-          for working as an analyst. My passion for technology and creativity
-          extends to various hobbies, such as PC building, programming,
-          photography, and digital art. I am a self-taught programmer, and I am
-          currently learning how to use next.js and solidity. It is my goal to
-          become a full-stack web3 developer.
+          Welcome to my website! A bit about me, I am a happy husband and father
+          of two, who works as an analyst. My passions for technology and
+          creativity extends to various hobbies, such as PC building,
+          programming, photography and digital art. I am a self-taught
+          programmer and I am currently learning how to use next.js and
+          solidity. My goal is to become a full stack web3 developer with an
+          emphasis on consumer protections and privacy.
         </p>
       </div>
     </div>
