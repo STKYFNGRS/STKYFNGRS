0747e5a9fcb5bb48e1f0159f5f4a20f68331a9c8
Alex M <contact@stkyfngrs.com>
2023-04-03 19:44:55 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -8,9 +8,7 @@ const AboutPage = () => {
           Welcome to my website! A bit about me, I am a happy husband and father
           of two, who works as an analyst. My passions for technology and
           creativity extends to various hobbies, such as PC building,
-          programming, photography and digital art. I am a self-taught
-          programmer and I am currently learning how to work with next.js and
-          solidity.
+          programming, photography and digital art.
         </p>
       </div>
     </div>
