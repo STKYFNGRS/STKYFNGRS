adb7189288b160ec317e05d5e1903986d0668b39
Alex M <contact@stkyfngrs.com>
2023-04-23 12:41:51 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -6,18 +6,18 @@ const AboutPage = () => {
       <div className="about-me-text">
         <p>
           Welcome to my website! My name is Alex and I live in Southern
-          California. A bit about me, I am a happy husband and father of two,
-          who works as an analyst. My passions for technology and creativity
-          extends to various hobbies, such as PC building, programming,
-          photography and digital art. The Learn tab at the top will have some
-          links I found interesting and helpful regarding web3, crypto and
-          development. When I got interested in Ethereum in 2021 I had never ran
-          a command prompt, used powershell or done anything technical in
-          computing for that matter, I had stricly been an end user up to this
-          point. The information in these links were instrumental on my web3
-          journey. The Mint page is a simple NFT minting dapp I built for the
-          pieces of my art I'd like to get onto the blockchain. Thanks for
-          stopping by, I hope you enjoy!
+          California. A bit about me, I am a happy husband and father of two. An
+          analyst by trade, my passions for technology and creativity extends to
+          various hobbies, such as PC building, programming, photography and
+          digital art. The Learn tab at the top will have some links I found
+          interesting and helpful regarding web3, crypto and development. When I
+          got interested in Ethereum in 2021 I had never ran a command prompt,
+          used powershell or done anything technical in computing for that
+          matter, I had stricly been an end user up to this point. The
+          information in these links were instrumental on my web3 journey. The
+          Mint page is a simple NFT minting dapp I built for the pieces of my
+          art I'd like to get onto the blockchain. Thanks for stopping by, I
+          hope you enjoy!
         </p>
       </div>
     </div>
