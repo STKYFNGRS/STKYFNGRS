f81248142c0927fa27b44610fad8fdeae9aea9d7
Alex M <contact@stkyfngrs.com>
2023-04-23 12:39:50 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -16,10 +16,8 @@ const AboutPage = () => {
           computing for that matter, I had stricly been an end user up to this
           point. The information in these links were instrumental on my web3
           journey. The Mint page is a simple NFT minting dapp I built for the
-          pieces of my art I'd like to get onto the blockchain. This is a 10k
-          collection comprised of a generative collection I created with a few
-          hundred of my favorite photographs that I've taken over the years
-          sprinkled in. Thanks for stopping by, I hope you enjoy!
+          pieces of my art I'd like to get onto the blockchain. Thanks for
+          stopping by, I hope you enjoy!
         </p>
       </div>
     </div>
