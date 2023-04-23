b11f964f08436fa610255404ae7f1db9336bcca1
Alex M <contact@stkyfngrs.com>
2023-04-23 14:39:52 -0700

    commit

--- pages/about.js
+++ pages/about.js
@@ -9,15 +9,7 @@ const AboutPage = () => {
           California. A bit about me, I am a happy husband and father of two. An
           analyst by trade, my passions for technology and creativity extends to
           various hobbies, such as PC building, programming, photography and
-          digital art. The Learn tab at the top will have some links I found
-          interesting and helpful regarding web3, crypto and development. When I
-          got interested in Ethereum in 2021 I had never ran a command prompt,
-          used powershell or done anything technical in computing for that
-          matter, I had stricly been an end user up to this point. The
-          information in these links were instrumental on my web3 journey. The
-          Mint page is a simple NFT minting dapp I built for the pieces of my
-          art I'd like to get onto the blockchain. Thanks for stopping by, I
-          hope you enjoy!
+          digital art. The Thanks for stopping by, I hope you enjoy!
         </p>
       </div>
     </div>
