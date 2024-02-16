38edc24e9f270e1b9b07e41d79b14e94e5a221a9
Alex M <contact@stkyfngrs.com>
2024-02-16 13:47:44 -0800

    commit

--- pages/about.js
+++ pages/about.js
@@ -5,11 +5,21 @@ const AboutPage = () => {
     <div className="about-me-container">
       <div className="about-me-text">
         <p>
-          Welcome to my website! My name is Alex and I live in Southern
-          California. A bit about me, I am a happy husband and father of two. An
-          analyst by trade, my passions for technology and creativity extends to
-          various hobbies, such as PC building, programming, photography and
-          digital art. Thanks for stopping by, I hope you enjoy!
+          Can we forget about the things I said when I was drunk? I didn't mean
+          to call you that I can't remember what was said, or what you threw at
+          me Please tell me Please tell me why My car is in the front yard And
+          I'm sleeping with my clothes on I came in through the window last
+          night And you're long gone, gone It's no surprise to me, I am my own
+          worst enemy 'Cause every now and then, I kick the living shit out of
+          me The smoke alarm is going off, and there's a cigarette Still burning
+          Please tell me why My car is in the front yard And I'm sleeping with
+          my clothes on I came in through the window last night And you're long
+          gone, gone Please tell me why My car is in the front yard And I'm
+          sleeping with my clothes on I came in through the window last night
+          it's no surprise to me, I am my own worst enemy (Ah-ooh) 'cause every
+          now and then, I kick the living shit out of me (Ah-ooh) can we forget
+          about the things I said when I was drunk? (Ah-ooh) I didn't mean to
+          call you that
         </p>
       </div>
     </div>
