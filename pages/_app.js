fe36e691366bd3ef96fe83e29a4da8008ec9303f
Alex M <contact@stkyfngrs.com>
2024-02-22 09:42:52 -0800

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -66,7 +66,6 @@ function MyApp({ Component, pageProps }) {
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
                     width="16"
                     height="16"
                     fill="currentColor"
